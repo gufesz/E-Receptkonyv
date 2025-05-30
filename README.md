486cdf4fe9a76f55f136d0e67a5758ce72d33cd2
moty63 <tmor553@gmail.com>
2025-05-30 10:54:22 +0200

    proba

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # the following disclaimer in the documentation and/or other materials
 # xdddd kuss
+
+#szepen beszelje man vele
--- php/login.php
+++ php/login.php
@@ -34,7 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Jelentkezz be a ReceptKönyvbe, hogy megoszthasd receptjeidet!">
-    <title>ReceptKönyv - Bejelentkezés</title>
+    <title>ReceptKöny - Bejelentkezés</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
